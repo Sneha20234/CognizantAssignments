034c459332c7132a9fa3be197fa24357973e6539
Snehalata Das <130863417+Sneha20234@users.noreply.github.com>
2024-08-16 10:03:14 +0530

    Delete Week_3/EmployeeManagementSystem directory

--- Week_3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.EmployeeManagementSystem.repositories;
-
-import com.example.EmployeeManagementSystem.entities.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
--- Week_3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/EmployeeProjection.java
+++
@@ -1,6 +0,0 @@
-package com.example.EmployeeManagementSystem;
-
-public interface EmployeeProjection {
-	  String getName();
-	  String getDepartmentName();
-}
